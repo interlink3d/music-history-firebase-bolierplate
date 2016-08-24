323a2f4e716441dda6cca823e6a01fc772bf30e0
Edgar Barajas <ebarajas05@gmail.com>
2016-08-24 12:06:13 -0500

    added all functions to song list

--- js/db-interaction.js
+++ js/db-interaction.js
@@ -10,23 +10,62 @@ let $ = require('jquery'),
 // ****************************************
 
 function getSongs(callback) {
-
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: 'https://music-history-54c84.firebaseio.com/songs.json',
+    }).done(function (songData) {
+      resolve(songData);
+    });
+  });
 }
 
 function addSong(songFormObj) {
-
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: 'https://music-history-54c84.firebaseio.com/songs.json',
+      type: 'POST',  // used for first time posting to DB
+      data: JSON.stringify(songFormObj),
+      dataType: 'json'
+    }).done(function (songId) {
+      resolve(songId);
+    });
+  });
 }
 
 function deleteSong(songId) {
-
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: `https://music-history-54c84.firebaseio.com/songs/${songId}.json`,
+      type: 'DELETE',
+    }).done(function (data) {
+      resolve(data);
+    });
+  });
 }
 
 function getSong(songId) {
-
+  return new Promise(function (resolve, reject) {
+  $.ajax({
+      url: `https://music-history-54c84.firebaseio.com/songs/${songId}.json`
+    }).done(function (songData) {
+      console.log("songData", songData);
+      resolve(songData);
+    }).fail(function(error) {
+      reject(error);
+    });
+  });  // end of promise
 }
 
 function editSong(songFormObj, songId) {
-
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: `https://music-history-54c84.firebaseio.com/songs/${songId}.json`,
+      type: 'PUT',  // used for updating something in the same spot
+      data: JSON.stringify(songFormObj)
+    }).done(function (data) {
+      resolve(data);
+    });
+  });
 }
 
 module.exports = {
--- js/main.js
+++ js/main.js
@@ -7,29 +7,66 @@ let $ = require('jquery'),
 
 // Using the REST API
 function loadSongsToDOM() {
-  console.log("Need to load some songs, Buddy");
+  $(".uiContainer--wrapper").html("");  // cleared the DOM before loading songs
+  db.getSongs()
+    .then(function(songData) {
+      var idArr = Object.keys(songData);   // Object.keys takes the keys for that object and puts them into an array
+      idArr.forEach(function(key) {
+        songData[key].id = key;           // using [] because it is a variable that we are passing in
+      });
+      console.log("songObject with ID added", songData);
+      templates.makeSongList(songData);
+    });
 }
 loadSongsToDOM(); //<--Move to auth section after adding login btn
 
 // Send newSong data to db then reload DOM with updated song data
 $(document).on("click", ".save_new_btn", function() {
-
+  let songObj = buildSongObj();
+  db.addSong(songObj)
+  .then(function (songId) {
+    console.log("song saved", songId);
+    loadSongsToDOM();
+  });
 });
 
 // Load and populate form for editing a song
 $(document).on("click", ".edit-btn", function () {
-
+  let songId = $(this).data("edit-id");
+  console.log("id?", songId);      // normally would use .attr but since template has data-edit-id we use .data
+  db.getSong(songId)
+  .then(function (song) {
+    return templates.songForm(song, songId);  // have to return a promise before chaining another .then
+  })
+  .then(function(finishedForm){
+    $(".uiContainer--wrapper").html(finishedForm);
+  });
 });
 
 //Save edited song to FB then reload DOM with updated song data
 $(document).on("click", ".save_edit_btn", function() {
-
+  let songObj = buildSongObj(),
+      songId = $(this).attr("id");
+  db.editSong(songObj, songId)
+  .then(function (data) {
+    loadSongsToDOM();
+  });
 });
 
 // Remove song then reload the DOM w/out new song
 $(document).on("click", ".delete-btn", function () {
-
+  let songId = $(this).data("delete-id");
+  db.deleteSong(songId)
+  .then(function (data) {
+    loadSongsToDOM();
+  });
 });
+
+// View songs from the list created in FB
+$(document).on("click", "#view-songs", function () {
+  loadSongsToDOM();
+});
+
 
 
 //***************************************************************
